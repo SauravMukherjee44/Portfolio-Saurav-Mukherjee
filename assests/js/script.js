ed1c61c39ac0f4f309a14bc25b690df6c00cd1c0
Saurav Mukherjee <72380920+SauravMukherjee44@users.noreply.github.com>
2022-08-22 16:40:47 +0530

    UI Updated

--- assests/js/script.js
+++ assests/js/script.js
@@ -53,7 +53,7 @@ function(){
 
 // <!-- typed js effect starts -->
     var typed = new Typed(".typing-text", {
-        strings: ["Android development", "Full-Stack development", "Machine Learning"],
+        strings: ["Full-Stack development", "Machine Learning", "Cloud Computing", "Tech Communities"],
         loop: true,
         typeSpeed: 50,
 		backSpeed: 25,
@@ -77,25 +77,6 @@ function fadeOut(){
 }
 window.onload = fadeOut;
 // pre loader end
-
-// disable developer mode
-document.onkeydown = function(e) {
-  if(e.keyCode == 123) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-     return false;
-  }
-}
 
 // Start of Tawk.to Live Chat
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
